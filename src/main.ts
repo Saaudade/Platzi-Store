7952fd39e76e2c626ea54ee6b63d66b8671813dd
Christian Tamayo <giby098@hotmail.com>
2021-10-31 10:20:28 -0500

    Uso de whitelist y forbinNonWhitelisted para evitar parámetros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina los campos extras y los ignora
+      forbidNonWhitelisted: true, // Da a conocer la alerta
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
