110dd091ba5b7fb0088360a3caaaaa5f68e040c2
Christian Tamayo <giby098@hotmail.com>
2021-10-30 22:36:05 -0500

    Se implemento el servicio para productos y se inyecto a su controlador correspondiente

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -14,17 +14,22 @@ import {
 
 import { Response } from 'express';
 
+import { ProductsService } from '../services/products.service';
+
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return {
-      message: `products: limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
-    };
+    // return {
+    //   message: `products: limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    // };
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -36,32 +41,29 @@ export class ProductsController {
 
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
-  getOne(@Res() response: Response, @Param('productId') productId: string) {
-    response.status(200).send({
-      message: `producto ${productId}`,
-    });
+  getOne(@Param('productId') productId: string) {
+    // response.status(200).send({
+    //   message: `producto ${productId}`,
+    // });
+    return this.productsService.findOne(+productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'Acción de crear',
-      payload,
-    };
+    // return {
+    //   message: 'Acción de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+    return this.productsService.update(id, payload);
   }
 
   @Delete(':id')
   delete(@Param('id') id: number) {
-    return {
-      id,
-    };
+    return this.productsService.remove(id);
   }
 }
