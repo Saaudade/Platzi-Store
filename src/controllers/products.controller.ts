ceea41c9d235bcef880e3fa7730c41d92f5bba7e
Christian Tamayo <giby098@hotmail.com>
2021-10-30 20:18:46 -0500

    Se eso la etiquete @HttpCode para dar respuesta de los estados junto con el uso de 'res'

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -7,7 +7,12 @@ import {
   Put,
   Query,
   Delete,
+  HttpCode,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
+
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
@@ -30,10 +35,11 @@ export class ProductsController {
   }
 
   @Get(':productId')
-  getOne(@Param('productId') productId: string) {
-    return {
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Res() response: Response, @Param('productId') productId: string) {
+    response.status(200).send({
       message: `producto ${productId}`,
-    };
+    });
   }
 
   @Post()
